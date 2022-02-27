b6a992b2eab0c611a86a0827a31a5c8286508ceb
hoangdh143 <huuhoang.dao@gmail.com>
2022-02-27 14:15:32 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+const fuzz = require('fuzzball');
+
 
 function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const handleInput = e => setInput(e.target.value);
+  const analyzeText = () => {
+      console.log(input);
+      const sentences = input.split(/[.,!;?\n]/).filter(sent => sent.length > 30);
+      const matchedSentences = sentences.map(sent => {
+          return {
+              source: sent,
+              matched: sentences.filter(sent2 => {
+                  const ratio = fuzz.token_set_ratio(sent, sent2);
+                  return ratio > 56 && ratio < 100;
+              }),
+          };
+      })
+      const result = matchedSentences.filter(sent => sent.matched.length > 0)
+          .map(sent => ("source: " + sent.source + "\n matched:" + sent.matched.join("\n ->")))
+          .join("\n ---- \n");
+      setOutput(result);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea value={input} onChange={handleInput}></textarea>
+      <button onClick={analyzeText}>Process</button>
+        <div style={{whiteSpace: "pre-line"}}>
+            {output}
+        </div>
     </div>
   );
 }
